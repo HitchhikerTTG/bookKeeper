ecdd6421c31b507c2b903d2f16cb380d9b109a20
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-11-09 11:25:51 +0000

    Jeszcze jedna poprawka

--- bookKeeper.php
+++ bookKeeper.php
@@ -125,8 +125,6 @@ $booksWithoutMetadata = [];
 foreach ($mobiFiles as $mobiFile) {
     $mobiFileName = getMobiFileName($mobiFile);
     if (isset($bookData[$mobiFileName])) {
-        $bookData[$mobiFileName]['httpLink'] = 'http://' . $_SERVER['HTTP_HOST'] . '/_ksiazki/' . $mobiFile;
-        $bookData[$mobiFileName]['httpsLink'] = 'https://' . $_SERVER['HTTP_HOST'] . '/_ksiazki/' . $mobiFile;
         $booksWithMetadata[] = $bookData[$mobiFileName];
     } else {
         $booksWithoutMetadata[] = $mobiFile;
@@ -485,3 +483,13 @@ echo '
         });
     </script>
 ';
+
+// Wczytanie Bootstrap z CDN
+echo '
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+';
+
+?>
